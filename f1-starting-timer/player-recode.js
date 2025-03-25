a449cd850cb7da0bced455ef4e83c1831f1cc25a
JinyeungKim <92693776+yashin20@users.noreply.github.com>
2025-03-26 02:37:35 +0900

    Update player-recode.js

---
+++ f1-starting-timer/player-recode.js
@@ -0,0 +1,34 @@
+/**
+ * Max Verstappen - 0.219 - https://www.youtube.com/shorts/eiLG3qEtiLs
+ * Valtteri Bottas - 0.040 - https://www.linkedin.com/posts/mordy-levine-ba2b65_exitstrategy-esops-esop-activity-7170430803653373954-HtxO
+ * Michael Schumacher - 0.160
+ * Lewis Hamilton - 0.200
+ * Nigel Mansell - 0.180
+ * Romain Grosjean - 00.234 - https://www.youtube.com/watch?v=6fgGJ-M6X2s
+ * Daniil Kvyat - 00.244
+ * Sergio Perez - 00.246
+ * Fernando Alonso - 00.285
+ * Marcus Ericsson - 00.245
+ * Pascal Wehrlein - 00.262
+ */
+
+const reactionTimes = new Map([
+  ["고양이", 0.020],
+  ["Valtteri Bottas", 0.040],
+  ["뱀", 0.070],
+  ["Michael Schumacher", 0.160],
+  ["Nigel Mansell", 0.180],
+  ["Lewis Hamilton", 0.200],
+  ["Max Verstappen", 0.219],
+  ["Romain Grosjean", 0.234],
+  ["Daniil Kvyat", 0.244],
+  ["Marcus Ericsson", 0.245],
+  ["Sergio Perez", 0.246],
+  ["Pascal Wehrlein", 0.262],
+  ["Fernando Alonso", 0.285]
+]);
+
+// // 데이터 출력
+// reactionTimes.forEach((time, driver) => {
+//   console.log(`${driver}: ${time}`);
+// });
